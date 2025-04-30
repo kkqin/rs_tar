3d14967456eaed4acb17ed3047439290f1a8cd28
3k <xxxx@gmail.com>
2025-04-30 22:35:55 +0800

    commit test

--- tests/pt_tests.rs
+++ tests/pt_tests.rs
@@ -2,8 +2,6 @@ use pt::base::{try_into_tarfile, ImageInfo, TarImage};
 
 #[test]
 fn test_pt_tar() {
-    //let path = r#"X:\gta5\tools_ng\bin\python\App\Lib\test\testtar.tar"#;
-    //let path = r#"C:\Program Files\Docker\Docker\resources\wsl\wsl-data.tar"#;
     let path = r#"C:\Program Files\Docker\Docker\resources\wsl\wsl-bootstrap.tar"#;
     let img = match TarImage::open(path) {
         Ok(img) => img,
